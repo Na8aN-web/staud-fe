f0aeb54cabeb145ff2d916f239aa88c65920461a
Hollajay <127396029+Hollajay@users.noreply.github.com>
2025-06-17 10:21:34 +0100

    custom button and input added

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        white: '#ffffff',
+        primarycolor: '#F68920',
+        secondarycolor: '#0B407A',
+        outline_grey: '#D9D9D9',
+        labelColor: '#E8E8E8'
+      }
+    },
   },
   plugins: [],
 }
